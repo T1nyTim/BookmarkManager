33ed06d6c06021fa473073fd353413854791b777
T1nyTim <t1nytim@example.com>
2026-04-16 21:17:33 +1200

    Add Clipboard Service

--- src/bookmark_manager/services/clipboard.py
+++ src/bookmark_manager/services/clipboard.py
@@ -1,2 +1,16 @@
+from typing import TYPE_CHECKING
+
+from PySide6.QtGui import QGuiApplication
+
+if TYPE_CHECKING:
+    from bookmark_manager.services.bookmark import BookmarkService
+
+
 class ClipboardService:
-    def copy() -> None: ...
+    def __init__(self, bookmark_service: BookmarkService) -> None:
+        self._bookmark_service = bookmark_service
+
+    def copy(self, bookmark_id: int, url: str) -> None:
+        clipboard = QGuiApplication.clipboard()
+        clipboard.setText(url)
+        self._bookmark_service.copy_bookmark(bookmark_id)
