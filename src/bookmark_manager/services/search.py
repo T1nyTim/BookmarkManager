532cbbed65bd397ed870880ad3a9d1172f6d65b1
T1nyTim <t1nytim@example.com>
2026-04-16 21:12:08 +1200

    Add Search Service

--- src/bookmark_manager/services/search.py
+++ src/bookmark_manager/services/search.py
@@ -1,2 +1,21 @@
+from typing import TYPE_CHECKING
+
+from bookmark_manager.domain.search import rank_bookmarks
+
+if TYPE_CHECKING:
+    from bookmark_manager.repositories.bookmark import BookmarkRepository
+    from bookmark_manager.repositories.bookmark_tag import BookmarkTagRepository
+
+
 class SearchService:
-    def search() -> None: ...
+    def __init__(self, bookmark_repo: BookmarkRepository, bookmark_tag_repo: BookmarkTagRepository) -> None:
+        self._bookmark_repo = bookmark_repo
+        self._bookmark_tag_repo = bookmark_tag_repo
+
+    def search(self, query: str) -> list:
+        bookmarks = self._bookmark_repo.list_all()
+        bookmark_id_to_tags = {}
+        for bookmark in bookmarks:
+            tags = self._bookmark_tag_repo.get_tags_for_bookmark(bookmark.bookmark_id)
+            bookmark_id_to_tags[bookmark.bookmark_id] = [tag.name_normalized for tag in tags]
+        return rank_bookmarks(bookmarks, bookmark_id_to_tags, query)
